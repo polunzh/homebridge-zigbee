3793432c35e6b5babd4e8808e9ce194bf58df915
zhenqiang <zhangzq@zigzen.cn>
2017-08-24 14:37:49 +0800

    添加容错处理

--- test.js
+++ test.js
@@ -1,4 +1,31 @@
-const SerialClient = require('./lib/serialclient.js');
+const OsramClient = require('./lib/osramclient');
 const util = require('./lib/util');
 
-console.log(util.generateCommand('FE0E050202d37a038800dd0000'));
+// console.log(util.generateCommand('FE0E050202d37a038800dd0000'));
+
+const osramclient = new OsramClient();
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
+osramclient.getBulbState('d37a', '03', () => console.log());
