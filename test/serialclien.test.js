d70776e72d5be8beb478e38ae187ff248e70d7b6
zhenqiang <zhangzq@zigzen.cn>
2017-11-08 15:40:54 +0800

    update

--- test/serialclien.test.js
+++ test/serialclien.test.js
@@ -2,5 +2,5 @@ const SerialClient = require('../lib/serialclient');
 const assert = require('assert');
 
 const serialClient = new SerialClient();
-const res = serialClient.parseData('fe0b02440283d60a880066fe1c02840283d60a2300040109000005000003000400150006000007');
+const res = serialClient.parseData('fe3b04810244f00a8800486f6e65794765656b20202020202020506f77657220');
 console.log(res);
